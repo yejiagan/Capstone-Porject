3cfe8b8ae564a047fca1ebdec360c7f12e8e11cf
yejiagan <58377126+yejiagan@users.noreply.github.com>
2019-12-01 01:25:56 -0500

    Add files via upload

---
+++ Capstone Project Week 1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is used for the IBM data science capstone project.
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
